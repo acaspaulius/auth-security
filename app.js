efb43cb0b2a6a5623e5df3f557ce0bcd6ee1ef29
Paulius Acas <acas@Pauliuss-MacBook-Pro.local>
2023-06-21 12:43:01 +0300

    Complete hashing passwords.

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -26,9 +23,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -48,7 +42,7 @@ app.post("/register", async function (req, res) {
   try {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password,
+      password: md5(req.body.password),
     });
 
     await newUser.save();
@@ -60,7 +54,7 @@ app.post("/register", async function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username })
     .then(function (foundUser) {
